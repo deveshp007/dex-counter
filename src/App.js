42368e3586926250864588bcc5b48a480984b270
Devesh Pandey <72290650+deveshp007@users.noreply.github.com>
2023-02-15 20:34:51 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function CounterApp() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    if(count <10){
+    setCount(prevCount => prevCount + 1);
+  }
+  };
+
+  const handleDecrement = () => {
+    if(count >0){
+    setCount(prevCount => prevCount - 1);
+  }
+  };
+
+  let color = 'green';
+  if (count >= 5 && count <= 9) {
+    color = 'blue';
+  } else if (count === 10) {
+    color = 'red';
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='mainhead'>DeX Counter</h1>
+      
+    
+      <h1 className='countNum' style={{ color: color }}>{count}</h1>
+      <div className='middle'>
+      <button className='btn btn-outline-success btn-lg' onClick={handleIncrement}>Increment</button>
+      <button className='btn btn-outline-danger btn-lg' onClick={handleDecrement}>Decrement</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default CounterApp;
